467c2189598714b69b8c0434c770d412d2ae64a8
Bright-Phiri <bphiri.aki@gmail.com>
2022-03-04 11:54:59 +0200

    first commit

--- app/models/lab_order.rb
+++ app/models/lab_order.rb
@@ -1,6 +1,6 @@
 class LabOrder < ApplicationRecord
   belongs_to :patient
-  has_one :result
+  has_one :result, dependent: :destroy
   validates :qrcode, :blood_type, :temperature, presence: true
   VALID_BLOOD_TYPES = ['Group A', 'Group B', 'Group AB', 'Group O']
   validates :qrcode, uniqueness: true
