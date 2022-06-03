3e763fbee10efe87511bc57d980c32afe932fc22
Bright-Phiri <bphiri.aki@gmail.com>
2022-06-03 07:31:47 +0200

    updated validations for username & contacts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true, uniqueness: true
-    validates :username, format: { without: /\s/, message: ' must contain no spaces' }
+    validates :username, presence: true, uniqueness: true, format: { without: /\s/, message: ' must contain no spaces' }
     validates :phone, presence: true, uniqueness: true,numericality: {only_integer: true}, format: {with: /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/}
     VALID_ROLES = ['Admin', 'Lab Assistant', 'HDA Personnel']
     validates :role, presence: true, inclusion: {in: VALID_ROLES}
