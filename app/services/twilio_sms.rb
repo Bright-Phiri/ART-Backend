6e82abf13ffac0709bb9f318f3fec833201ef00d
Bright_Phiri <bphiri.aki@gmail.com>
2022-07-30 13:36:49 +0200

    twilio sms errors fixing

---
+++ app/services/twilio_sms.rb
@@ -0,0 +1,17 @@
+class TwilioSms
+    attr_reader :phone_number, :message
+
+    def initialize(phone_number, message)
+      @phone_number = phone_number
+      @message = message
+    end
+
+    def call
+      twilio_client = Twilio::REST::Client.new Rails.application.credentials.twilio_sid, Rails.application.credentials.twilio_token
+      twilio_client.api.account.messages.create(
+      from: Rails.application.credentials.twilio_phone_number,
+      to: phone_number, 
+      body: message
+      )
+    end
+  end
