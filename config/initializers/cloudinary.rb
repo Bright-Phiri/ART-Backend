9bb581e1f8ace28d70351f70afdb60c2dd8ec5ed
Bright_Phiri <bphiri.aki@gmail.com>
2022-08-02 11:34:25 +0200

    fixing cloudnary service key in production

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-    config.cloud_name = Rails.application.credentials.cloud_name
-    config.api_key = Rails.application.credentials.api_key
-    config.api_secret = Rails.application.credentials.api_secret
-    config.secure = true
-end
