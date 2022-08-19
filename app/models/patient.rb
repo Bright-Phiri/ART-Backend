a56a6b589a49789c300626acbc4cdc783a6563e3
Bright Phiri <bphiri.aki@gmail.com>
2022-08-19 14:58:01 +0200

    added phone number validation on Mw numbers only

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,7 +5,7 @@ class Patient < ApplicationRecord
     validate :date_of_birth_cannot_be_in_the_future
     VALID_GENDERS = ['Male','Female']
     validates :gender, inclusion: { in: VALID_GENDERS}
-    validates :phone, uniqueness: true ,numericality: { only_integer: true}, format: {with: /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{7})\z/, message: ' number is invalid'}
+    validates :phone, uniqueness: true ,numericality: { only_integer: true}, format: {with: /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/, message: ' number is invalid'}
     after_commit :broadcast_data, on: [:create, :destroy]
 
     scope :male_patients,->{where(gender: 'Male')}
