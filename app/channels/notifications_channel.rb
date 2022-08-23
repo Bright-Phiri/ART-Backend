cec9afcef38cf54713254db01dc5fe9675291f2e
Bright Phiri <bphiri.aki@gmail.com>
2022-08-23 13:17:52 +0200

    stream from notification channel right after sub

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,9 +1,16 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
       stream_from "notifications_channel"
+      NotificationsChannel.unverified_lab_orders
   end
 
   def unsubscribed
     stop_all_streams
   end
+
+  def self.unverified_lab_orders
+    data = {unverified_lab_orders_count: LabOrder.unverified_lab_orders.count}.as_json
+    NotificationRelayJob.perform_later(data)
+  end
+
 end
