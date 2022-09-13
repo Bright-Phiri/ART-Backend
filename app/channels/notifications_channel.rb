24524a4fa715d092dece74e75f3777d7e737c58b
Bright Phiri <bphiri.aki@gmail.com>
2022-09-13 13:46:37 +0200

    feat: broadcasting current year statistics and notifications right after subscription

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,15 +1,15 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
     stream_from "notifications_channel"
-    unverified_lab_orders
   end
 
   def unsubscribed
     stop_all_streams
   end
 
-  def unverified_lab_orders
+  after_subscribe do
     data = {unverified_lab_orders_count: LabOrder.unverified_lab_orders.count}.as_json
     NotificationRelayJob.perform_later(data)
   end
+
 end
