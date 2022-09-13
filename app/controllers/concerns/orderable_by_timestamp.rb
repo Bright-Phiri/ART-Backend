956b609f228894fe797c2d0f16b065b527daad17
Bright Phiri <bphiri.aki@gmail.com>
2022-09-13 11:06:55 +0200

    feat: allow year selection when broadcasting statistics

--- app/controllers/concerns/orderable_by_timestamp.rb
+++ app/controllers/concerns/orderable_by_timestamp.rb
@@ -2,10 +2,10 @@ module OrderableByTimestamp
     extend ActiveSupport::Concern
 
     included do 
-        scope :by_earliest_created, -> { order(created_at: :asc) }
-        scope :by_recently_created, -> { order(:created_at).reverse_order }
-        scope :by_earliest_updated, -> { order(updated_at: :asc) }
-        scope :by_recently_updated, -> { order(:updated_at).reverse_order }
-        scope :created_in,->(year){where('extract(year from created_at) = ?', year)}
+      scope :by_earliest_created, -> { order(created_at: :asc) }
+      scope :by_recently_created, -> { order(:created_at).reverse_order }
+      scope :by_earliest_updated, -> { order(updated_at: :asc) }
+      scope :by_recently_updated, -> { order(:updated_at).reverse_order }
+      scope :created_in,->(year){where('extract(year from created_at) = ?', year)}
     end
 end
