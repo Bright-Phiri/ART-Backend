ebf2b6bdee6693cbe0bc5a59a1c1aa97021f85fa
Bright Phiri <bphiri.aki@gmail.com>
2022-09-13 11:36:09 +0200

    feat: allow year selection when broadcasting statistics

--- app/controllers/concerns/orderable_by_timestamp.rb
+++ app/controllers/concerns/orderable_by_timestamp.rb
@@ -7,5 +7,6 @@ module OrderableByTimestamp
       scope :by_earliest_updated, -> { order(updated_at: :asc) }
       scope :by_recently_updated, -> { order(:updated_at).reverse_order }
       scope :created_in,->(year){where('extract(year from created_at) = ?', year)}
+      scope :created_in_month_of,->(month){where('extract(month from created_at) = ?', month )}
     end
 end
