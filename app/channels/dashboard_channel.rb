4192e4895435ba5fd7d897406973b604941bb90f
Bright Phiri <bphiri.aki@gmail.com>
2022-09-13 13:26:03 +0200

    feat: broadcasting current year statistics right after subscription

--- app/channels/dashboard_channel.rb
+++ app/channels/dashboard_channel.rb
@@ -1,15 +1,15 @@
 class DashboardChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "dashboard_channel"
-    DashboardChannel.statistics
+      stream_from "dashboard_channel"
+      statistics
   end
 
   def unsubscribed
-    stop_all_streams
+      stop_all_streams
   end
 
-  def self.statistics
-    data = {res: 'all', lab_orders: LabOrder.unscoped.statistics, users: User.where(role: 'Admin').invert_where.count, patients: Patient.count, lab_orders_count: LabOrder.count, results: Result.count}.as_json
-    DashboardSocketDataJob.perform_later(data)
+  def statistics
+      data = {res: 'all', lab_orders: LabOrder.unscoped.statistics, users: User.where(role: 'Admin').invert_where.count, patients: Patient.count, lab_orders_count: LabOrder.count, results: Result.count}.as_json
+      DashboardSocketDataJob.perform_later(data)
   end
 end
--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,15 +1,15 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
       stream_from "notifications_channel"
-      NotificationsChannel.unverified_lab_orders
+      unverified_lab_orders
   end
 
   def unsubscribed
-    stop_all_streams
+      stop_all_streams
   end
 
-  def self.unverified_lab_orders
-    data = {unverified_lab_orders_count: LabOrder.unverified_lab_orders.count}.as_json
-    NotificationRelayJob.perform_later(data)
+  def unverified_lab_orders
+      data = {unverified_lab_orders_count: LabOrder.unverified_lab_orders.count}.as_json
+      NotificationRelayJob.perform_later(data)
   end
 end
