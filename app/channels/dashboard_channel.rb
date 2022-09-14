98bb314a78e4a655e1be452b06cccd8c143dafff
Bright Phiri <bphiri.aki@gmail.com>
2022-09-14 07:37:15 +0200

    feat: broadcasting current year statistics and notifications to all channel subscribers  right after subscription

--- app/channels/dashboard_channel.rb
+++ app/channels/dashboard_channel.rb
@@ -11,4 +11,5 @@ class DashboardChannel < ApplicationCable::Channel
     data = {res: 'all', lab_orders: LabOrder.unscoped.statistics, users: User.where(role: 'Admin').invert_where.count, patients: Patient.count, lab_orders_count: LabOrder.count, results: Result.count}.as_json
     DashboardSocketDataJob.perform_later(data)
   end
+  
 end
