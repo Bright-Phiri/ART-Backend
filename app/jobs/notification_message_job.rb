95ee498349c9c7568244d9f38f3069d9938a6032
Bright Phiri <bphiri.aki@gmail.com>
2022-09-16 11:44:51 +0200

    fix: fixing sending sms

--- app/jobs/notification_message_job.rb
+++ app/jobs/notification_message_job.rb
@@ -2,6 +2,6 @@ class NotificationMessageJob < ApplicationJob
   queue_as :notification_messages
 
   def perform(phone_number, message)
-      TwilioSms(phone_number, message).call
+      TwilioSms.call(phone_number, message)
   end
 end
