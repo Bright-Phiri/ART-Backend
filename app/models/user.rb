193be75648baec049de1431e19539dee2d3d30b8
Bright Phiri <bphiri.aki@gmail.com>
2022-09-23 13:59:40 +0200

    added email validator & updated model level validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     validates :phone, phone: true, uniqueness: true, numericality: {only_integer: true}
     VALID_ROLES = ['Admin', 'Lab Assistant', 'HDA Personnel']
     validates :role, presence: true, inclusion: {in: VALID_ROLES}
-    validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i ,message: ' Entered is invalid'}
+    validates :email, presence: true, uniqueness: true, email: true
     validates :password, length: {in: 6..8}
     scope :lab_assistants,->{where(role: 'Lab Assistant')}
     scope :hda_personnels,->{where(role: 'HDA Personnel')}
