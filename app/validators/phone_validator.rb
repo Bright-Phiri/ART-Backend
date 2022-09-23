713b3fd4f889cc34cfd09df8f8d653b07bf5d6d2
Bright Phiri <bphiri.aki@gmail.com>
2022-09-23 14:13:33 +0200

    added email validator & updated model level validations

--- app/validators/phone_validator.rb
+++ app/validators/phone_validator.rb
@@ -1,5 +1,7 @@
 class PhoneValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
-        record.errors.add attribute, (options[:message] || "number is invalid") unless value =~ /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/
+        unless value =~ /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/
+          record.errors.add attribute, (options[:message] || "number is invalid")
+        end
     end
 end
