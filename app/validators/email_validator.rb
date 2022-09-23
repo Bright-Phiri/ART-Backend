09d835f436fa8271fc03c5012becd1fb893f6f3c
Bright Phiri <bphiri.aki@gmail.com>
2022-09-23 14:14:09 +0200

    added email validator & updated model level validations

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,8 +1,5 @@
 class EmailValidator < ActiveModel::EachValidator
-
     def validate_each(record, attribute, value)
-        unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-            record.errors.add attribute, (options[:message] || " Entered is invalid")
-        end
+        record.errors.add attribute, (options[:message] || " Entered is invalid") unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     end
 end
--- app/validators/phone_validator.rb
+++ app/validators/phone_validator.rb
@@ -1,7 +1,5 @@
 class PhoneValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, value)
-        unless value =~ /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/
-          record.errors.add attribute, (options[:message] || "number is invalid")
-        end
+        record.errors.add attribute, (options[:message] || "number is invalid") unless value =~ /\A(\+?(265|0){1}(1|88[0-9]|99[0-9]|98[0-9]|90[0-9]){1}[0-9]{6})\z/
     end
 end
