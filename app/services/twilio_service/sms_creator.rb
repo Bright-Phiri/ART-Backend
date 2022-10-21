cde6338d09437d8c028124765ee7a020766e9398
Bright Phiri <bphiri.aki@gmail.com>
2022-10-21 18:00:29 +0200

    refactor: refactoring  test results code

--- app/services/twilio_service/sms_creator.rb
+++ app/services/twilio_service/sms_creator.rb
@@ -1,24 +1,22 @@
-module TwilioService
-  class SmsCreator < ApplicationService
-    attr_reader :phone_number, :message
+class TwilioService::SmsCreator < ApplicationService
+  attr_reader :phone_number, :message
 
-    def initialize(phone_number, message)
-      @phone_number = phone_number
-      @message = message
-    end
+  def initialize(phone_number, message)
+    @phone_number = phone_number
+    @message = message
+  end
 
-    def call
-      send_text_message
-    end
+  def call
+    send_text_message
+  end
 
-    private
-    def send_text_message
-      twilio_client = Twilio::REST::Client.new Rails.application.credentials.twilio_sid, Rails.application.credentials.twilio_token
-      twilio_client.api.account.messages.create(
-      from: Rails.application.credentials.twilio_phone_number,
-      to: phone_number, 
-      body: message
-      )
-    end
+  private
+  def send_text_message
+    twilio_client = Twilio::REST::Client.new Rails.application.credentials.twilio_sid, Rails.application.credentials.twilio_token
+    twilio_client.api.account.messages.create(
+    from: Rails.application.credentials.twilio_phone_number,
+    to: phone_number, 
+    body: message
+    )
   end
 end
