168b4b6b1237e31e47ce766b1af55dd91b4d876b
Bright Phiri <bphiri.aki@gmail.com>
2022-11-29 16:03:48 +0200

    fix: fixing broadcasting to all channel subscribers right after subscription

--- app/channels/dashboard_channel.rb
+++ app/channels/dashboard_channel.rb
@@ -8,7 +8,7 @@ class DashboardChannel < ApplicationCable::Channel
   end
 
   on_subscribe do
-    data = {res: 'all', lab_orders: LabOrder.unscoped.statistics, users: User.where(role: 'Admin').invert_where.count, patients: Patient.count, lab_orders_count: LabOrder.count, results: Result.count}.as_json
+    data = {res: 'all', lab_orders: LabOrder.unscoped.statistics, users: User.where(role: 'Admin').invert_where.count, patients: Patient.count, lab_orders_count: LabOrder.count, results: Result.count}
     DashboardSocketDataJob.perform_later(data)
   end
 end
--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -8,7 +8,7 @@ class NotificationsChannel < ApplicationCable::Channel
   end
 
   on_subscribe do
-    data = {unverified_lab_orders_count: LabOrder.unverified_lab_orders.count}.as_json
+    data = {unverified_lab_orders_count: LabOrder.unverified_lab_orders.count}
     NotificationRelayJob.perform_later(data)
   end
 end
