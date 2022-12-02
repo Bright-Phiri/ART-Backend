6579a2bba6ab1bc62ab0267b47399f2b4c12126e
Bright Phiri <bphiri.aki@gmail.com>
2022-12-02 15:16:20 +0200

    rails best practices

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Result < ApplicationRecord
+  include Filterable
   belongs_to :lab_order
   validates :patient_name, :blood_type, :hiv_res, :conducted_by, presence: true
   validates :tisuue_res, presence: true, allow_blank: true
-  include Filterable
   after_commit :publish_to_dashboard, on: [:create, :destroy]
 
   private
