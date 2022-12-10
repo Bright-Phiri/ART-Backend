c257bd13662eab01e2cbf3ff03111de49aa6a2a3
Bright Phiri <bphiri.aki@gmail.com>
2022-12-10 05:41:00 +0200

    updated model level validations

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Patient < ApplicationRecord
-  VALID_GENDERS = ['Male', 'Female'].freeze
-  has_many :lab_orders, inverse_of: :patient, dependent: :destroy
   scope :male_patients, -> { where(gender: VALID_GENDERS.first) }
   scope :female_patients, -> { male_patients.invert_where }
   include Filterable
+  VALID_GENDERS = ['Male', 'Female'].freeze
+  has_many :lab_orders, inverse_of: :patient, dependent: :destroy
   validates :first_name, :last_name, :gender, :dob, :district, :village, :phone, :location, presence: true
   validate :date_of_birth_cannot_be_in_the_future
   validates :gender, inclusion: { in: VALID_GENDERS }
